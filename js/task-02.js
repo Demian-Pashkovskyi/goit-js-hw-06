f24cf35e9b0035eb410be930261619a3a38a21b8
Demian Pashkovskyi <skyangely4@gmail.com>
2022-05-22 14:31:01 +0300

    HW-06 task-02

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,47 @@
 const ingredients = [
-	'Potatoes',
-	'Mushrooms',
-	'Garlic',
-	'Tomatos',
-	'Herbs',
-	'Condiments',
- ];
+  'Potatoes',
+  'Mushrooms',
+  'Garlic',
+  'Tomatos',
+  'Herbs',
+  'Condiments',
+];
+
+// const list = document.querySelector('#ingredients');
+
+
+// ingredients.forEach(item => {
+//     const itemEl = document.createElement('li');
+//     itemEl.textContent = item;
+//     itemEl.classList.add('item');
+
+//     list.append(itemEl);
+// });
+// console.log(list);
+
+
+// const list = document.querySelector(`#ingredients`);
+// const elements = [];
+// for (let i = 0; i < ingredients.length; i +1) {
+// 	const option = ingredients[i];
+// 	const navEl = document.createElement(`li`);
+// 	navEl.classList.add("item");
+// 	navEl.textContent = option;
+	
+// }	
+
+// console.log(elements);
+// list.append(...elements);
+
+const list = document.querySelector(`#ingredients`);	
+const elements = ingredients.map(option => {
+	const navEl = document.createElement(`li`);
+	navEl.classList.add("item");
+	navEl.textContent = option;
+
+	return navEl;
+
+});
+
+console.log(list);
+list.append(...elements);
